485b15eacec3a0379b00e26dd2b70c06ecdb13f8
alfygitau <alfygitau@gmail.com>
2024-11-07 15:04:09 +0300

    changes

--- src/pages/contributors/Contributors.jsx
+++ src/pages/contributors/Contributors.jsx
@@ -4,6 +4,7 @@ import { getCounties } from "../../sdk/market/market";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { Select, Space, Modal, Pagination } from "antd";
+import { getRoles } from "../../sdk/auth/auth";
 
 const Contributors = () => {
   const navigate = useNavigate();
@@ -28,6 +29,11 @@ const Contributors = () => {
   const [username, setUsername] = useState("");
   const [role, setRole] = useState("");
   const [totalFscCount, setTotalFscCount] = useState(0);
+
+  const [market, setMarket] = useState("");
+  const [roles, setRoles] = useState([]);
+  const [markets, setMarkets] = useState([]);
+  const [countyMarkets, setCountyMarkets] = useState([]);
 
   const handleChange = (value) => {
     setSelectedWards(value);
@@ -92,12 +98,24 @@ const Contributors = () => {
 
   useEffect(() => {
     getAllCounties();
+    fetchRoles();
   }, []);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
   const [selectedUser, setSelectedUser] = useState("");
   const [selectedEditUser, setSelectedEditUser] = useState("");
+
+  const showCreateModal = () => {
+    setIsCreateModalOpen(true);
+  };
+  const handleCreateOk = () => {
+    setIsCreateModalOpen(true);
+  };
+  const handleCreateCancel = () => {
+    setIsCreateModalOpen(false);
+  };
 
   const showModal = (id) => {
     setSelectedUser(id);
@@ -157,6 +175,44 @@ const Contributors = () => {
 
   const onShowSizeChange = (current, pageSize) => {
     console.log(current, pageSize);
+  };
+
+  const handleCreateFsc = async () => {
+    try {
+      const payload = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        msisdn: `254${msisdn.substring(1)}`,
+        username: `254${msisdn.substring(1)}`,
+        roleId: Number(role),
+        marketId: Number(market),
+        fsc: Number(role) === 4 ? true : false,
+      };
+      const response = await createAUser(payload);
+      if (response.status === 201 || response.status === 200) {
+        setFirstName("");
+        setLastName("");
+        setUsername("");
+        setEmail("");
+        setMsisdn("");
+      }
+      toast.success("User created successfully");
+      setIsModalOpen(false);
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error?.message);
+    }
+  };
+
+  const fetchRoles = async () => {
+    try {
+      const response = await getRoles();
+      if (response.status === 200) {
+        setRoles(response.data.data);
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
   };
 
   return (
@@ -220,10 +276,126 @@ const Contributors = () => {
           />
         </div>
       </Modal>
+      <Modal
+        centered
+        width={700}
+        title="Add farm service centers or user"
+        open={isCreateModalOpen}
+        onOk={handleCreateFsc}
+        onCancel={handleCreateCancel}
+      >
+        <div className="w-full">
+          <div className="bg-white">
+            <div className="w-full flex justify-between h-full">
+              <div className="w-[49%]">
+                <div className="w-full flex flex-col gap-[5px] mb-[20px]">
+                  <label htmlFor="msisdn">First Name</label>
+                  <input
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    placeholder="Enter your first name"
+                    className="h-[50px] w-full text-[14px] border px-[10px] border-gray-400 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-primary-110"
+                  />
+                </div>
+                <div className="w-full flex flex-col gap-[5px] mb-[20px]">
+                  <label htmlFor="msisdn">Last Name</label>
+                  <input
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    placeholder="Enter your last name"
+                    className="h-[50px] w-full text-[14px] border px-[10px] border-gray-400 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-primary-110"
+                  />
+                </div>
+                <div className="w-full flex flex-col gap-[5px] mb-[20px]">
+                  <label htmlFor="msisdn">Phone number</label>
+                  <input
+                    type="text"
+                    value={msisdn}
+                    onChange={(e) => setMsisdn(e.target.value)}
+                    placeholder="Enter your username"
+                    className="h-[50px] w-full text-[14px] border px-[10px] border-gray-400 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-primary-110"
+                  />
+                </div>
+                <div className="w-full flex flex-col gap-[5px] mb-[20px]">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Enter your username"
+                    className="h-[50px] w-full text-[14px] border px-[10px] border-gray-400 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-primary-110"
+                  />
+                </div>
+              </div>
+              <div className="w-[49%]">
+                <div className="w-full flex flex-col gap-[5px] mb-[20px]">
+                  <label htmlFor="msisdn">County</label>
+                  <select
+                    type="text"
+                    value={county}
+                    onChange={(e) => handleCountyChange(e.target.value)}
+                    placeholder="Enter your phone number"
+                    className="h-[50px] w-[100%] rounded text-[#000] text-[14px] border px-[10px] border-gray-400 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-primary-110"
+                  >
+                    <option value="">Select your county</option>
+                    {counties?.length > 0 &&
+                      counties?.map((county) => (
+                        <option key={county.countyId} value={county.countyId}>
+                          {county.countyName}
+                        </option>
+                      ))}
+                  </select>
+                </div>
+                <div className="w-full flex flex-col gap-[5px] mb-[20px]">
+                  <label htmlFor="role">Role</label>
+                  <select
+                    value={role}
+                    onChange={(e) => setRole(e.target.value)}
+                    placeholder="Enter your username"
+                    className="h-[50px] w-full text-[14px] border px-[10px] border-gray-400 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-primary-110"
+                  >
+                    {roles?.length > 0 &&
+                      roles.map((role) => (
+                        <option key={role?.roleId} value={role?.roleId}>
+                          {role?.title}
+                        </option>
+                      ))}
+                  </select>
+                </div>
+                {Number(role) === 4 && (
+                  <div className="w-full flex flex-col gap-[5px] mb-[20px]">
+                    <label htmlFor="market">Markets</label>
+                    <select
+                      value={market}
+                      onChange={(e) => {
+                        setMarket(e.target.value);
+                      }}
+                      placeholder="Select your market"
+                      className="h-[50px] w-full text-[14px] border px-[10px] border-gray-400 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-primary-110"
+                    >
+                      <option value="">Select market</option>
+                      {countyMarkets?.map((market) => (
+                        <option id={market?.marketId} value={market?.marketId}>
+                          {market?.title}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
       <div className="flex items-center my-[20px] text-[13px] justify-between">
         <p className="text-[15px] font-semibold">All Farm Service Centers</p>
         <div>
-          <button className="h-[40px] bg-[#00b300] px-[20px] rounded text-white">
+          <button
+            onClick={showCreateModal}
+            className="h-[40px] bg-[#00b300] px-[20px] rounded text-white"
+          >
             Add Fsc
           </button>
         </div>
@@ -294,14 +466,14 @@ const Contributors = () => {
         <div className="flex text-[13px] font-bold border-b-2 h-[45px] items-center">
           <p className="w-[5%]">Id</p>
           <p className="w-[10%] truncate px-[10px]">Name</p>
-          <p className="w-[15%] truncate px-[10px]">email</p>
+          <p className="w-[10%] truncate px-[10px]">email</p>
           <p className="w-[10%] truncate px-[10px]">Phone number</p>
           <p className="w-[10%] truncate px-[10px]">Market</p>
           <p className="w-[10%] truncate px-[10px]">County</p>
           <p className="w-[10%] truncate px-[10px]">Subcounty</p>
           <p className="w-[10%] truncate px-[10px]">Ward</p>
           <p className="w-[10%] truncate px-[10px]">Points</p>
-          <p className="w-[10%] truncate px-[10px]">Action</p>
+          <p className="w-[15%] truncate px-[10px]">Action</p>
         </div>
         {isLoading && (
           <div className="my-[20px] flex items-center justify-center min-h-[500px] w-full">
@@ -335,7 +507,7 @@ const Contributors = () => {
               <p className="w-[10%] truncate px-[10px]">
                 {item.firstName} {item.lastName}
               </p>
-              <p className="w-[15%] truncate px-[10px]">{item.email}</p>
+              <p className="w-[10%] truncate px-[10px]">{item.email}</p>
               <p className="w-[10%] truncate px-[10px]">{item.msisdn}</p>
               <p className="w-[10%] truncate px-[10px]">{item.market}</p>
               <p className="w-[10%] truncate px-[10px]">{item.county}</p>
@@ -344,50 +516,41 @@ const Contributors = () => {
               <p className="w-[10%] truncate px-[10px]">
                 {item.marketPointsBalance}
               </p>
-              <div className="w-[10%] flex items-center gap-[10px] truncate">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="cursor-pointer"
+              <div className="w-[15%] flex items-center gap-[10px] truncate">
+                <div
                   onClick={() => showEditModal(item)}
+                  className="flex items-center justify-center gap-[5px] text-[12px] bg-[#0096FF] px-[10px] text-white rounded"
                 >
-                  <path
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m14.304 4.844l2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565l6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"
-                  />
-                </svg>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="cursor-pointer"
-                >
-                  <g fill="none" stroke="currentColor" stroke-width="2">
-                    <circle cx="12" cy="12" r="3" />
-                    <path d="M20.188 10.934c.388.472.582.707.582 1.066c0 .359-.194.594-.582 1.066C18.768 14.79 15.636 18 12 18c-3.636 0-6.768-3.21-8.188-4.934c-.388-.472-.582-.707-.582-1.066c0-.359.194-.594.582-1.066C5.232 9.21 8.364 6 12 6c3.636 0 6.768 3.21 8.188 4.934Z" />
-                  </g>
-                </svg>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="cursor-pointer"
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="14"
+                    viewBox="0 0 12 12"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M7.736 1.56a1.914 1.914 0 0 1 2.707 2.708l-.234.234l-2.707-2.707zm-.941.942L1.65 7.646a.5.5 0 0 0-.136.255l-.504 2.5a.5.5 0 0 0 .588.59l2.504-.5a.5.5 0 0 0 .255-.137l5.145-5.145z"
+                    />
+                  </svg>
+                  Edit
+                </div>
+                <div
                   onClick={() => showModal(item?.farmServiceCenterId)}
+                  className="flex items-center justify-center gap-[5px] text-[12px] bg-[#D22B2B] px-[10px] text-white rounded"
                 >
-                  <path
-                    fill="currentColor"
-                    fill-rule="evenodd"
-                    d="m18.412 6.5l-.801 13.617A2 2 0 0 1 15.614 22H8.386a2 2 0 0 1-1.997-1.883L5.59 6.5H3.5v-1A.5.5 0 0 1 4 5h16a.5.5 0 0 1 .5.5v1zM10 2.5h4a.5.5 0 0 1 .5.5v1h-5V3a.5.5 0 0 1 .5-.5M9 9l.5 9H11l-.4-9zm4.5 0l-.5 9h1.5l.5-9z"
-                  />
-                </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="14"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1"
+                    />
+                  </svg>
+                  Delete
+                </div>
               </div>
             </div>
           ))}
