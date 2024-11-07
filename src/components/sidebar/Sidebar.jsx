20fd33b2fc57c13d1edc813a96280260a5665dde
alfygitau <alfygitau@gmail.com>
2024-11-07 15:50:38 +0300

    changes

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -147,7 +147,7 @@ const Sidebar = () => {
     }
   };
   return (
-    <div className="flex w-[80%] mx-auto h-full flex-col">
+    <div className="flex w-[100%] px-[20px] mx-auto h-full flex-col">
       <div className="h-[100px] flex mb-[30px] items-center">
         <div className="w-full h-[80px] flex items-center border-b">
           <img
@@ -170,31 +170,6 @@ const Sidebar = () => {
         />
       </div>
       <div className="flex flex-col text-white px-[20px] h-full mb-[30px] justify-end sm:hidden gap-[20px]">
-        {/* <NavLink
-          end
-          to="/dashboard/partners"
-          activeClassName="active-link"
-          className="flex items-center gap-[10px]"
-          style={({ isActive }) => ({
-            color: isActive ? "blue" : "white",
-            fontWeight: isActive ? "bold" : "normal",
-          })}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
-            height="18"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill="currentColor"
-              fill-rule="evenodd"
-              d="M8.716.315a1 1 0 0 0-1.432 0L6.646.97a1 1 0 0 1-.988.265l-.88-.248a1 1 0 0 0-1.24.716l-.226.886a1 1 0 0 1-.723.723l-.886.225a1 1 0 0 0-.716 1.24l.248.881a1 1 0 0 1-.265.988l-.655.638a1 1 0 0 0 0 1.432l.655.639a1 1 0 0 1 .265.987l-.248.88a1 1 0 0 0 .716 1.24l.886.226a1 1 0 0 1 .723.723l.225.886a1 1 0 0 0 1.24.717l.881-.248a1 1 0 0 1 .988.264l.638.655a1 1 0 0 0 1.432 0l.639-.655a1 1 0 0 1 .987-.264l.88.248a1 1 0 0 0 1.24-.717l.226-.886a1 1 0 0 1 .723-.723l.886-.225a1 1 0 0 0 .717-1.24l-.248-.88a1 1 0 0 1 .264-.988l.655-.639a1 1 0 0 0 0-1.432l-.655-.638a1 1 0 0 1-.264-.988l.248-.88a1 1 0 0 0-.717-1.24l-.886-.226a1 1 0 0 1-.723-.723l-.225-.886a1 1 0 0 0-1.24-.716l-.88.248A1 1 0 0 1 9.354.97zm3.057 5.975a.75.75 0 0 0-1.042-1.08L6.597 9.202L5.28 7.887A.75.75 0 0 0 4.22 8.95l1.839 1.834a.75.75 0 0 0 1.05.01z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          <span>Our partners</span>
-        </NavLink> */}
         <p
           onClick={handleLogout}
           className="flex cursor-pointer text-white items-center gap-[10px]"
