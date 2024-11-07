ac6bc3eff31d163aa5b6bb3827ea1b96c42521fe
alfygitau <alfygitau@gmail.com>
2024-11-07 15:24:20 +0300

    changes

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -249,7 +249,7 @@ const Homepage = () => {
       </div>
       <div className="flex w-full justify-between">
         <div className="flex items-center w-[49%] justify-between">
-          <div className="h-[80px] w-[250px] sm:w-[250px] bg-white shadow-md flex items-center gap-[10px] p-[15px]">
+          <div className="h-[80px] w-[30%] sm:w-[30%] bg-white shadow-md flex items-center gap-[10px] p-[15px]">
             <div className="w-[30%]">
               <div className="h-[60px] w-[60px]  bg-[#E8EFFB] flex items-center justify-center">
                 <svg
@@ -276,7 +276,7 @@ const Homepage = () => {
               </div>
             </div>
           </div>
-          <div className="h-[80px] w-[250px] sm:w-[250px] bg-white shadow-md flex items-center gap-[10px] p-[15px]">
+          <div className="h-[80px] w-[30%] sm:w-[30%] bg-white shadow-md flex items-center gap-[10px] p-[15px]">
             <div className="w-[30%]">
               <div className="h-[60px] w-[60px]  bg-[#FCF5E5] flex items-center justify-center">
                 <svg
@@ -307,9 +307,7 @@ const Homepage = () => {
               </div>
             </div>
           </div>
-        </div>
-        <div className="flex items-center w-[49%] justify-between">
-          <div className="h-[80px] w-[250px] sm:w-[250px] bg-white shadow-md flex items-center gap-[10px] p-[15px]">
+          <div className="h-[80px] w-[30%] sm:w-[30%] bg-white shadow-md flex items-center gap-[10px] p-[15px]">
             <div className="w-[30%]">
               <div className="h-[60px] w-[60px]  bg-[#E5F6FB] flex items-center justify-center">
                 <svg
@@ -338,7 +336,9 @@ const Homepage = () => {
               </div>
             </div>
           </div>
-          <div className="h-[80px] w-[250px] sm:w-[250px] bg-white shadow-md flex items-center gap-[10px] p-[15px]">
+        </div>
+        <div className="flex items-center w-[49%] justify-between">
+          <div className="h-[80px] w-[30%] sm:w-[30%] bg-white shadow-md flex items-center gap-[10px] p-[15px]">
             <div className="w-[30%]">
               <div className="h-[60px] w-[60px]  bg-[#E8EFFB] flex items-center justify-center">
                 <svg
@@ -354,9 +354,63 @@ const Homepage = () => {
                 </svg>
               </div>
             </div>
-            <div className="flex w-[70%] flex-col gap-[10px] items-start">
-              <p className="text-[14px] truncate text-center w-full">
-                PRICE ENTRIES
+            <div className="flex w-[70%] h-[60px] flex-col justify-center items-start">
+              <p className="text-[13px] text-center w-full">
+                FARM GATE PRICE ENTRIES
+              </p>
+              <div className="flex w-full items-center justify-center">
+                <p className="text-[16px] text-oldGod font-semibold">
+                  {summaries?.marketPricesEntries}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="h-[80px] w-[30%] sm:w-[30%] bg-white shadow-md flex items-center gap-[10px] p-[15px]">
+            <div className="w-[30%]">
+              <div className="h-[60px] w-[60px]  bg-[#E8EFFB] flex items-center justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M19.388.405a.605.605 0 0 0-1.141.399c.929 2.67-.915 4.664-2.321 5.732l-.568-.814c-.191-.273-.618-.5-.95-.504l-3.188.014a2.16 2.16 0 0 0-1.097.338L.729 12.157a1.01 1.01 0 0 0-.247 1.404l4.269 6.108c.32.455.831.4 1.287.082l9.394-6.588c.27-.191.582-.603.692-.918l.998-3.145c.11-.314.043-.793-.148-1.066l-.346-.496c1.888-1.447 3.848-4.004 2.76-7.133m-4.371 9.358a1.61 1.61 0 0 1-2.24-.396a1.614 1.614 0 0 1 .395-2.246a1.61 1.61 0 0 1 1.868.017c-.272.164-.459.26-.494.275a.606.606 0 0 0 .259 1.153q.13 0 .257-.059q.292-.137.619-.33a1.62 1.62 0 0 1-.664 1.586"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="flex w-[70%] h-[60px] flex-col justify-center items-start">
+              <p className="text-[13px] text-center w-full">
+                RETAIL PRICE ENTRIES
+              </p>
+              <div className="flex w-full items-center justify-center">
+                <p className="text-[18px] text-oldGod font-semibold">
+                  {summaries?.marketPricesEntries}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="h-[80px] w-[30%] sm:w-[30%] bg-white shadow-md flex items-center gap-[10px] p-[15px]">
+            <div className="w-[30%]">
+              <div className="h-[60px] w-[60px]  bg-[#E8EFFB] flex items-center justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M19.388.405a.605.605 0 0 0-1.141.399c.929 2.67-.915 4.664-2.321 5.732l-.568-.814c-.191-.273-.618-.5-.95-.504l-3.188.014a2.16 2.16 0 0 0-1.097.338L.729 12.157a1.01 1.01 0 0 0-.247 1.404l4.269 6.108c.32.455.831.4 1.287.082l9.394-6.588c.27-.191.582-.603.692-.918l.998-3.145c.11-.314.043-.793-.148-1.066l-.346-.496c1.888-1.447 3.848-4.004 2.76-7.133m-4.371 9.358a1.61 1.61 0 0 1-2.24-.396a1.614 1.614 0 0 1 .395-2.246a1.61 1.61 0 0 1 1.868.017c-.272.164-.459.26-.494.275a.606.606 0 0 0 .259 1.153q.13 0 .257-.059q.292-.137.619-.33a1.62 1.62 0 0 1-.664 1.586"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="flex w-[70%] h-[60px] flex-col justify-center items-start">
+              <p className="text-[13px] text-center w-full">
+                WHOLESALE PRICE ENTRIES
               </p>
               <div className="flex w-full items-center justify-center">
                 <p className="text-[18px] text-oldGod font-semibold">
